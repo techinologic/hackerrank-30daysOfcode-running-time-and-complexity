1cb2b0988af06ed4a220a9e70e3982e49172d1f9
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-14 10:45:27 -0400

    final with documentation

--- src/Solution.java
+++ src/Solution.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Solution {
 
     static boolean isPrime(int n) {
-        //check if n is a multiple of 2, knowing if even nums are prime is trivial
+        //check if n is a multiple of 2,
+        // knowing if even nums are prime is trivial
         if (n % 2 == 0) return false;
         //then check the odds
         for (int i = 3; i * i <= n; i += 2) {
@@ -18,12 +19,16 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
 
+        //stores input into int array for individual processing
         int[] nums = new int[input];
         for (int i = 0; i < input; i++) {
             nums[i] = sc.nextInt();
         }
         sc.close();
 
+        // checks each element of the array for primality
+        // "for each i, inside of nums arrays, if isPrime
+        // is true, print appropriate message"
         for (int i :
                 nums) {
             if (isPrime(i)) {
